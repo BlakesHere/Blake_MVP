b4069e5b3391c286218a73abff28c3d4e82986ae
iamblake <brbarkmanb@gmail.com>
2023-04-11 21:51:25 -0500

    mvp initial

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Blake_MVP
 
-
 https://www.youtube.com/watch?v=DihOP19LQdg&ab_channel=BeaufortTek
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express")
+const waxRoutes = require('./wax/routes')
+
 const app = express()
 const port = 3000
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("hello hello hello hello UH")
 });
+
+app.use('/api/wax', waxRoutes)
 
 app.listen(port, () => console.log(`app listening on ${port}`))
